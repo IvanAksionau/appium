91b4992cd962bf269689decc7ff4414b9d946d01
ivan.aksionau <ivanionov2015@mail.ru>
2022-06-14 14:22:26 +0300

    test

--- src/main/java/ivan/demo/com/utils/BrowserStackCloudUtil.java
+++ src/main/java/ivan/demo/com/utils/BrowserStackCloudUtil.java
@@ -35,7 +35,7 @@ public final class BrowserStackCloudUtil {
 
     public static String uploadApkFile(String filePath) {
         Process process;
-        String command = "curl -u \"ivan_vOIhrM:qFr7kmY16GrsXyPhNwmN\" -X POST \"https://api-cloud.browserstack.com/app-automate/upload\" -F \"file=@/var/jenkins_home/workspace/appiumTest/src/main/resources/ApiDemosDebug.apk\"";
+        String command = buildUploadApkCommand(filePath);
         try {
             LOGGER.info("Please wait while the APK is uploading to a Browserstack resource...");
             LOGGER.info("Upload command is: " + command);// TODO: 5/20/2022 delete after jenkins issue is fixed
@@ -45,7 +45,7 @@ public final class BrowserStackCloudUtil {
         }
         String appId = extractAppUrl(process);
         LOGGER.info("APK is uploaded to a Browserstack with ID: " + appId);
-        return "bs://d52293e24d0dc85cfeaec2bb716cea1b42f55119";
+        return appId;
     }
 
     private static String readProcess(Process process, String processType) {
